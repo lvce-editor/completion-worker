eca54f3e71d43fa3c71a6596df108a48ede9a98b
Le Vivilet <levivlet@pm.me>
2025-05-27 23:37:34 +0200

    lint

--- packages/completion-worker/src/parts/EditorCompletionOpenDetails/EditorCompletionOpenDetails.ts
+++ packages/completion-worker/src/parts/EditorCompletionOpenDetails/EditorCompletionOpenDetails.ts
@@ -2,19 +2,18 @@ import type { CompletionDetailState } from '../CompletionDetailState/CompletionD
 import type { CompletionState } from '../CompletionState/CompletionState.ts'
 import * as GetCompletionDetailBounds from '../GetCompletionDetailBounds/GetCompletionDetailBounds.ts'
 
-export const openDetails = async (state: CompletionState): Promise<CompletionState> => {
-  // TODO avoid closure
-  // @ts-ignore
-  const newStateGenerator = (state: CompletionDetailState): CompletionDetailState => {
-    const borderSize = 1
-    const newestState: CompletionDetailState = {
-      ...state,
-      content: 'abc',
-      ...GetCompletionDetailBounds.getCompletionDetailBounds(state, borderSize),
-    }
-    return newestState
+// @ts-ignore
+const newStateGenerator = (state: CompletionDetailState): CompletionDetailState => {
+  const borderSize = 1
+  const newestState: CompletionDetailState = {
+    ...state,
+    content: 'abc',
+    ...GetCompletionDetailBounds.getCompletionDetailBounds(state, borderSize),
   }
+  return newestState
+}
 
+export const openDetails = async (state: CompletionState): Promise<CompletionState> => {
   // TODO ask editor worker to add completion details widget
   return state
 }
