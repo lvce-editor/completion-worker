6d75ba22435a3722e81f7834f3dd8cbdb7ba5f3b
Le Vivilet <levivlet@pm.me>
2025-05-27 20:31:10 +0200

    feature: add renderItems function

--- packages/completion-worker/src/parts/GetRenderer/GetRenderer.ts
+++ packages/completion-worker/src/parts/GetRenderer/GetRenderer.ts
@@ -3,6 +3,7 @@ import * as DiffType from '../DiffType/DiffType.ts'
 import * as RenderBounds from '../RenderBounds/RenderBounds.ts'
 import * as RenderContent from '../RenderContent/RenderContent.ts'
 import * as RenderEventListeners from '../RenderEventListeners/RenderEventListeners.ts'
+import * as RenderItems from '../RenderItems/RenderItems.ts'
 
 export const getRenderer = (diffType: number): Renderer => {
   switch (diffType) {
@@ -12,6 +13,8 @@ export const getRenderer = (diffType: number): Renderer => {
       return RenderBounds.renderBounds
     case DiffType.RenderEventListeners:
       return RenderEventListeners.renderEventListeners
+    case DiffType.RenderItems:
+      return RenderItems.renderItems
     default:
       throw new Error('unknown renderer')
   }
