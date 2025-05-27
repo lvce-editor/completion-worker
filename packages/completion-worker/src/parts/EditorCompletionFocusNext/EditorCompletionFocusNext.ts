108fad4cc138330fc318e4ac3d9dfe4f88ae28ad
Le Vivilet <levivlet@pm.me>
2025-05-27 23:19:50 +0200

    feature: simplify keybindings

--- packages/completion-worker/src/parts/EditorCompletionFocusNext/EditorCompletionFocusNext.ts
+++ packages/completion-worker/src/parts/EditorCompletionFocusNext/EditorCompletionFocusNext.ts
@@ -2,6 +2,7 @@ import { type CompletionState } from '../CompletionState/CompletionState.ts'
 import * as EditorCompletionFocusIndex from '../EditorCompletionFocusIndex/EditorCompletionFocusIndex.ts'
 
 export const focusNext = (state: CompletionState): CompletionState => {
-  const nextIndex = state.focusedIndex + 1
+  const { focusedIndex } = state
+  const nextIndex = focusedIndex + 1
   return EditorCompletionFocusIndex.focusIndex(state, nextIndex)
 }
