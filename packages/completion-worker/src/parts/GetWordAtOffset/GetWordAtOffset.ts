8bd80822d2ea2974cbdc61dc5d9523fdcd0bad17
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-27 19:42:50 +0200

    feature: ask editor worker for word at offset (#16)

--- packages/completion-worker/src/parts/GetWordAtOffset/GetWordAtOffset.ts
+++ packages/completion-worker/src/parts/GetWordAtOffset/GetWordAtOffset.ts
@@ -1,4 +1,6 @@
+import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
+
 export const getWordAtOffset = async (editorUid: number): Promise<string> => {
-  // TODO ask editor worker
-  return ''
+  // @ts-ignore
+  return EditorWorker.invoke('Editor.getWordAtOffset2', editorUid)
 }
