fc0570418b03d9197b0e5a1d2101cc789555cca7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-27 22:36:49 +0200

    feature: add focusindex commands (#38)

--- packages/completion-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/completion-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,10 @@
 import * as WrapCommand from '../CompletionStates/CompletionStates.ts'
 import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
+import * as FocusFirst from '../EditorCompletionFocusFirst/EditorCompletionFocusFirst.ts'
+import * as FocusIndex from '../EditorCompletionFocusIndex/EditorCompletionFocusIndex.ts'
+import * as EditorCompletionFocusNext from '../EditorCompletionFocusNext/EditorCompletionFocusNext.ts'
+import * as EditorCompletionFocusPrevious from '../EditorCompletionFocusPrevious/EditorCompletionFocusPrevious.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
 import * as HandleEditorType from '../HandleEditorType/HandleEditorType.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
@@ -17,4 +21,8 @@ export const commandMap = {
   'Completions.terminate': Terminate.terminate,
   'Completions.initialize': Initialize.initialize,
   'Completions.handleEditorType': HandleEditorType.handleEditorType,
+  'Completions.focusIndex': WrapCommand.wrapCommand(FocusIndex.focusIndex),
+  'Completions.focusFirst': WrapCommand.wrapCommand(FocusFirst.focusFirst),
+  'Completions.focusNext': WrapCommand.wrapCommand(EditorCompletionFocusNext.focusNext),
+  'Completions.focusPrevious': WrapCommand.wrapCommand(EditorCompletionFocusPrevious.focusPrevious),
 }
