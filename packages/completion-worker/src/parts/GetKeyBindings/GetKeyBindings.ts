9fbcf067541f0c6d5e8b650133914ea457906ffe
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-27 23:05:58 +0200

    feature: wrap keybindings (#42)

--- packages/completion-worker/src/parts/GetKeyBindings/GetKeyBindings.ts
+++ packages/completion-worker/src/parts/GetKeyBindings/GetKeyBindings.ts
@@ -1,37 +1,45 @@
 import * as KeyCode from '../KeyCode/KeyCode.ts'
 import * as KeyModifier from '../KeyModifier/KeyModifier.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
+import * as WidgetId from '../WidgetId/WidgetId.ts'
 
-export const getKeyBindings = (): readonly any[] => {
+const getCommand = (shortId: string, uid: number): any => {
+  return {
+    command: 'Editor.executeWidgetCommand',
+    args: ['Completion', `EditorCompletion.${shortId}`, uid, WidgetId.Completion],
+  }
+}
+
+export const getKeyBindings = (uid: number): readonly any[] => {
   return [
     {
       key: KeyCode.DownArrow,
-      command: 'EditorCompletion.focusNext',
+      ...getCommand('focusNext', uid),
       when: WhenExpression.FocusEditorCompletions,
     },
     {
       key: KeyCode.UpArrow,
-      command: 'EditorCompletion.focusPrevious',
+      ...getCommand('focusPrevious', uid),
       when: WhenExpression.FocusEditorCompletions,
     },
     {
       key: KeyCode.Enter,
-      command: 'EditorCompletion.selectCurrent',
+      ...getCommand('selectCurrent', uid),
       when: WhenExpression.FocusEditorCompletions,
     },
     {
       key: KeyCode.End,
-      command: 'EditorCompletion.focusLast',
+      ...getCommand('focusLast', uid),
       when: WhenExpression.FocusEditorCompletions,
     },
     {
       key: KeyCode.Home,
-      command: 'EditorCompletion.focusFirst',
+      ...getCommand('focusFirst', uid),
       when: WhenExpression.FocusEditorCompletions,
     },
     {
       key: KeyModifier.CtrlCmd | KeyCode.Space,
-      command: 'EditorCompletion.toggleDetails',
+      ...getCommand('toggleDetails', uid),
       when: WhenExpression.FocusEditorCompletions,
     },
   ]
