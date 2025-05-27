5136262b89caae168c460899f965965419be3ea8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-27 20:52:06 +0200

    feature: remove unused ts ignore (#30)

--- packages/completion-worker/src/parts/GetWordAtOffset/GetWordAtOffset.ts
+++ packages/completion-worker/src/parts/GetWordAtOffset/GetWordAtOffset.ts
@@ -1,6 +1,5 @@
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 
 export const getWordAtOffset = async (editorUid: number): Promise<string> => {
-  // @ts-ignore
   return EditorWorker.invoke('Editor.getWordAtOffset2', editorUid)
 }
