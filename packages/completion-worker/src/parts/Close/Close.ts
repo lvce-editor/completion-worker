e63feec684f49caa19829160d376d7432d8dfd34
Le Vivilet <levivlet@pm.me>
2025-05-31 11:42:26 +0200

    clear focus

--- packages/completion-worker/src/parts/Close/Close.ts
+++ packages/completion-worker/src/parts/Close/Close.ts
@@ -1,7 +1,8 @@
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 import * as WidgetId from '../WidgetId/WidgetId.ts'
+import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const close = async (editorUid: number): Promise<void> => {
   // @ts-ignore
-  await EditorWorker.invoke('Editor.closeWidget2', editorUid, WidgetId.Completion, 'Completions')
+  await EditorWorker.invoke('Editor.closeWidget2', editorUid, WidgetId.Completion, 'Completions', WhenExpression.FocusEditorCompletions)
 }
