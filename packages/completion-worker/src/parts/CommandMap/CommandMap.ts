14e34ee4db910e5e7c95de611d246326acb87a8e
Le Vivilet <levivlet@pm.me>
2025-05-31 11:29:41 +0200

    feature: add dispose command

--- packages/completion-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/completion-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,7 @@
 import * as WrapCommand from '../CompletionStates/CompletionStates.ts'
 import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
+import * as Dispose from '../Dispose/Dispose.ts'
 import * as FocusFirst from '../EditorCompletionFocusFirst/EditorCompletionFocusFirst.ts'
 import * as FocusIndex from '../EditorCompletionFocusIndex/EditorCompletionFocusIndex.ts'
 import * as EditorCompletionFocusNext from '../EditorCompletionFocusNext/EditorCompletionFocusNext.ts'
@@ -21,6 +22,7 @@ import * as Terminate from '../Terminate/Terminate.ts'
 export const commandMap = {
   'Completions.create': Create.create,
   'Completions.diff2': Diff2.diff2,
+  'Completions.dispose': Dispose.dispose,
   'Completions.focusFirst': WrapCommand.wrapCommand(FocusFirst.focusFirst),
   'Completions.focusIndex': WrapCommand.wrapCommand(FocusIndex.focusIndex),
   'Completions.focusNext': WrapCommand.wrapCommand(EditorCompletionFocusNext.focusNext),
