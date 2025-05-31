57942ff330982f185e382c796051d99b60e9d597
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-31 11:30:28 +0200

    feature: add dispose command (#53)

--- packages/completion-worker/src/parts/CompletionStates/CompletionStates.ts
+++ packages/completion-worker/src/parts/CompletionStates/CompletionStates.ts
@@ -1,4 +1,4 @@
 import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
 import type { CompletionState } from '../CompletionState/CompletionState.ts'
 
-export const { get, set, wrapCommand } = ViewletRegistry.create<CompletionState>()
+export const { get, set, wrapCommand, dispose } = ViewletRegistry.create<CompletionState>()
