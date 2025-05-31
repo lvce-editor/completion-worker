84cc88d609307f944a314e1c000fa75954bc2c37
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-31 11:05:08 +0200

    feature: ask editor worker to apply changes (#51)

--- packages/completion-worker/src/parts/Select/Select.ts
+++ packages/completion-worker/src/parts/Select/Select.ts
@@ -1,24 +1,13 @@
 import type { CompletionState } from '../CompletionState/CompletionState.ts'
+import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
+import { getEdits } from '../GetEdits/GetEdits.ts'
 
 export const select = async (state: CompletionState, completionItem: any): Promise<CompletionState> => {
-  // TODO ask editor worker to apply changes and close completion widget
-  // const changes = await getEdits(state, completionItem)
-  // TODO ask editor worker to remove widget
-  // const index = state.widgets
-  //   .indexOf
-  //   // ViewletModuleId.EditorCompletion
-  //   ()
-  // if (index !== -1) {
-  //   state.widgets.splice(index, 1)
-  //   state.completionState = EditorCompletionState.None
-  //   state.completionUid = 0
-  // }
-  // // TODO dispose completion widget
-  // // TODO apply edit in editor worker instead of asking renderer worker
-  // // await RendererWorker.invoke('Viewlet.dispose', state.uid)
-  // const { widgets } = state
-  // const newWidgets = RemoveEditorWidget.removeEditorWidget(widgets, WidgetId.Completion)
-  // const intermediateEditor = await EditorCommandApplyEdit.applyEdit(state, changes)
+  const { editorUid, leadingWord } = state
+  const changes = await getEdits(editorUid, leadingWord, completionItem)
+  // @ts-ignore
+  await EditorWorker.invoke('Editor.applyEdit2', editorUid, changes)
+  // TODO remove completion widget from editor
   return {
     ...state,
   }
