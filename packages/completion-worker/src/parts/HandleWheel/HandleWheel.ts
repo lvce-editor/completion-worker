8f22fe899abcf432cc8e64b6d656f9ad65d82bc9
Le Vivilet <levivlet@pm.me>
2025-06-03 13:14:36 +0200

    remove log

--- packages/completion-worker/src/parts/HandleWheel/HandleWheel.ts
+++ packages/completion-worker/src/parts/HandleWheel/HandleWheel.ts
@@ -5,6 +5,5 @@ import * as SetDeltaY from '../SetDeltaY/SetDeltaY.ts'
 export const handleWheel = <K, T extends VirtualListState<K>>(state: T, deltaMode: number, deltaY: number): T => {
   Assert.number(deltaMode)
   Assert.number(deltaY)
-  console.log('delta', deltaMode, deltaY)
   return SetDeltaY.setDeltaY(state, state.deltaY + deltaY)
 }
