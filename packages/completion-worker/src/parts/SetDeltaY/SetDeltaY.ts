eafb803bf0f3926470d851f91cd58e41dc3d7cfb
Le Vivilet <levivlet@pm.me>
2025-06-03 10:00:05 +0200

    feature: move offsets computation into separate file

--- packages/completion-worker/src/parts/SetDeltaY/SetDeltaY.ts
+++ packages/completion-worker/src/parts/SetDeltaY/SetDeltaY.ts
@@ -1,24 +1,19 @@
 import type { VirtualListState } from '../VirtualListState/VirtualListState.ts'
 import * as Assert from '../Assert/Assert.ts'
-import * as Clamp from '../Clamp/Clamp.ts'
-import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOfVisibleItems.ts'
+import * as GetNewOffsets from '../GetNewOffsets/GetNewOffsets.ts'
 
 export const setDeltaY = <K, T extends VirtualListState<K>>(state: T, value: number): T => {
   Assert.object(state)
   Assert.number(value)
   const { itemHeight, finalDeltaY, deltaY, height, headerHeight } = state
-  const listHeight = height - headerHeight
-  const newDeltaY = Clamp.clamp(value, 0, finalDeltaY)
-  if (deltaY === newDeltaY) {
+  const { newDeltaY, newMaxLineY, newMinLineY, modified } = GetNewOffsets.getNewOffsets(itemHeight, finalDeltaY, deltaY, height, headerHeight, value)
+  if (!modified) {
     return state
   }
-  // TODO when it only moves by one px, extensions don't need to be rerendered, only negative margin
-  const minLineY = Math.floor(newDeltaY / itemHeight)
-  const maxLineY = minLineY + GetNumberOfVisibleItems.getNumberOfVisibleItems(listHeight, itemHeight)
   return {
     ...state,
     deltaY: newDeltaY,
-    minLineY,
-    maxLineY,
+    minLineY: newMinLineY,
+    maxLineY: newMaxLineY,
   }
 }
