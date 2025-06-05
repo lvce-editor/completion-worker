26215f05d84694ded959c8e13b3067a6cc25287f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-05 09:31:57 +0200

    feature: enable more e2e tests (#113)
    * feature: enable more e2e tests
    
    * test
    
    * skip

--- packages/e2e/src/editor.completion-empty.ts
+++ packages/e2e/src/editor.completion-empty.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'editor.completion-empty'
 
-export const skip = 1
-
 export const test: Test = async ({ Extension, FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/editor.completion-empty')
@@ -20,7 +18,7 @@ export const test: Test = async ({ Extension, FileSystem, Workspace, Main, Edito
   // assert
   const completions = Locator('.EditorCompletion')
   await expect(completions).toBeVisible()
-  await expect(completions).toHaveText('No Results')
+  await expect(completions).toHaveText('No Suggestions')
   await expect(completions).toHaveCSS('top', '75px')
   await expect(completions).toHaveCSS('left', '0px')
 }
