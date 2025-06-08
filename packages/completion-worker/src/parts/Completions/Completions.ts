e73c3264a5678e2c1c6a974789a24dd136c7708d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-08 22:23:02 +0200

    feature: handle completions error more gracyfully (#123)

--- packages/completion-worker/src/parts/Completions/Completions.ts
+++ packages/completion-worker/src/parts/Completions/Completions.ts
@@ -1,24 +1,16 @@
 import type { CompletionItem } from '../CompletionItem/CompletionItem.ts'
-import * as Assert from '../Assert/Assert.ts'
 import * as ExtensionHostCompletion from '../ExtensionHostCompletion/ExtensionHostCompletion.ts'
 import * as GetOffsetAtCursor from '../GetOffsetAtCursor/GetOffsetAtCursor.ts'
+import * as Logger from '../Logger/Logger.ts'
 
 // TODO possible to do this with events/state machine instead of promises -> enables canceling operations / concurrent calls
 export const getCompletions = async (editorUid: number, editorLanguageId: string): Promise<readonly CompletionItem[]> => {
-  const offset = GetOffsetAtCursor.getOffsetAtCursor(editorUid)
-  const completions = await ExtensionHostCompletion.executeCompletionProvider(editorUid, editorLanguageId, offset)
-  return completions
-}
-
-// TODO don't send unnecessary parts of completion item like matches
-export const resolveCompletion = async (editorUid: number, name: string, completionItem: any): Promise<any> => {
   try {
-    Assert.string(name)
-    Assert.object(completionItem)
     const offset = GetOffsetAtCursor.getOffsetAtCursor(editorUid)
-    const resolvedCompletionItem = await ExtensionHostCompletion.executeResolveCompletionItem(editorUid, offset, name, completionItem)
-    return resolvedCompletionItem
-  } catch {
-    return undefined
+    const completions = await ExtensionHostCompletion.executeCompletionProvider(editorUid, editorLanguageId, offset)
+    return completions
+  } catch (error) {
+    Logger.error(`Failed to get completions: ${error}`)
+    return []
   }
 }
