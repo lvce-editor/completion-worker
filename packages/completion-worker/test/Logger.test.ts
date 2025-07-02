5ab61457bb10a0d9135434ba700d249885211ee4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-02 18:55:46 +0200

    feature: simplify port code (#183)

--- packages/completion-worker/test/Logger.test.ts
+++ packages/completion-worker/test/Logger.test.ts
@@ -2,6 +2,7 @@ import { test, expect, jest } from '@jest/globals'
 import { error } from '../src/parts/Logger/Logger.ts'
 
 test('error should log to console.error', () => {
+  // @ts-ignore
   const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {})
   error('test error')
   expect(consoleErrorSpy).toHaveBeenCalledWith('test error')
@@ -9,6 +10,7 @@ test('error should log to console.error', () => {
 })
 
 test('error should handle non-string messages', () => {
+  // @ts-ignore
   const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {})
   const errorObj = new Error('test error')
   error(errorObj)
