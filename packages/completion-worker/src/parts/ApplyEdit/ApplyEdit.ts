e0002f6a8b914f23f20a35b2f710b393e807d291
Le Vivilet <levivlet@pm.me>
2025-08-19 14:40:32 +0200

    feature: simplify applyEdit

--- packages/completion-worker/src/parts/ApplyEdit/ApplyEdit.ts
+++ packages/completion-worker/src/parts/ApplyEdit/ApplyEdit.ts
@@ -2,6 +2,5 @@ import type { Change } from '../Change/Change.ts'
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 
 export const applyEdit = async (editorUid: number, changes: readonly Change[]): Promise<void> => {
-  // @ts-ignore
-  await EditorWorker.invoke('Editor.applyEdit2', editorUid, changes)
+  await EditorWorker.applyEdit(editorUid, changes)
 }
