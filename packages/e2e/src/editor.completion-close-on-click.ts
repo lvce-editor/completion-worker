91c101c2a7325f9cce4b463646a9715850bf68c0
Le Vivilet <levivilet@pm.me>
2025-09-21 09:50:17 +0200

    feature: improve close test

--- packages/e2e/src/editor.completion-close-on-click.ts
+++ packages/e2e/src/editor.completion-close-on-click.ts
@@ -2,7 +2,9 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'editor.completion-close-on-click'
 
-export const test: Test = async ({ Extension, FileSystem, Workspace, Main, Editor, Locator, expect }) => {
+export const skip = 1
+
+export const test: Test = async ({ Command, Extension, FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/editor.completion-one-result')
   await Extension.addWebExtension(extensionUri)
@@ -11,14 +13,16 @@ export const test: Test = async ({ Extension, FileSystem, Workspace, Main, Edito
   await Workspace.setPath(tmpDir)
   await Main.openUri(`${tmpDir}/file1.xyz`)
   await Editor.setCursor(0, 0)
-
-  // act
   await Editor.openCompletion()
-
-  // assert
   const completions = Locator('.EditorCompletion')
   await expect(completions).toBeVisible()
   const items = Locator('.EditorCompletionItem')
   await expect(items).toHaveCount(1)
   await expect(items).toHaveText('test')
+
+  // act
+  await Command.execute(`Editor.handleClickAtPosition`, 0, 0, 0)
+
+  // assert
+  await expect(completions).toBeHidden()
 }
