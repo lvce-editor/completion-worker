7db626b928a64929d0c3e202246326ca16d59b50
Le Vivilet <levivilet@pm.me>
2025-09-25 15:03:55 +0200

    feature: add pointerdown event

--- packages/completion-worker/src/parts/GetEventListeners/GetEventListeners.ts
+++ packages/completion-worker/src/parts/GetEventListeners/GetEventListeners.ts
@@ -1,3 +1,4 @@
+import { EventExpression } from '@lvce-editor/constants'
 import type { DomEventListener } from '../DomEventListener/DomEventListener.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
@@ -5,8 +6,13 @@ export const getEventListeners = (): readonly DomEventListener[] => {
   return [
     {
       name: DomEventListenerFunctions.HandleWheel,
-      params: ['EditorCompletion.handleWheel', 'event.deltaMode', 'event.deltaY'],
+      params: ['EditorCompletion.handleWheel', EventExpression.DeltaMode, EventExpression.DeltaY],
       passive: true,
+    },
+    {
+      name: DomEventListenerFunctions.HandlePointerDown,
+      params: ['EditorCompletion.handlePoinerDown', EventExpression.ClientX, EventExpression.ClientY],
+      preventDefault: true,
     },
   ]
 }
