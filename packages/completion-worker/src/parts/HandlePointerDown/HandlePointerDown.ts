a9d31c0855a284feac319f7abfc2552e1afe8b40
Le Vivilet <levivilet@pm.me>
2025-09-25 15:15:46 +0200

    lint

--- packages/completion-worker/src/parts/HandlePointerDown/HandlePointerDown.ts
+++ packages/completion-worker/src/parts/HandlePointerDown/HandlePointerDown.ts
@@ -1,4 +1,4 @@
-import { CompletionState } from '../CompletionState/CompletionState.ts'
+import type { CompletionState } from '../CompletionState/CompletionState.ts'
 
 export const handlePointerDown = (state: CompletionState, clientX: number, clientY: number): CompletionState => {
   return state
