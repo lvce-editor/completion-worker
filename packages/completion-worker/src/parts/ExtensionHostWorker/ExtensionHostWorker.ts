f4ee8621aaafe4b250c3ca4e3aea83823aacecb2
Le Vivilet <levivilet@pm.me>
2025-09-25 16:33:11 +0200

    tsc

--- packages/completion-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
+++ packages/completion-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
@@ -1,6 +1,6 @@
 import { ExtensionHost } from '@lvce-editor/rpc-registry'
 
 export const { invoke, set, dispose } = ExtensionHost
-export const registerMockRpc = (ExtensionHost as any).registerMockRpc
+export const { registerMockRpc } = ExtensionHost as any
 
 export { type MockRpc } from '@lvce-editor/rpc'
