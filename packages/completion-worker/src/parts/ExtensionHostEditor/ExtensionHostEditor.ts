300d60672e346a2831e559934329e57027666ff0
Le Vivilet <levivilet@pm.me>
2026-01-15 11:50:21 +0100

    feature: use editor worker module directly (#316)

--- packages/completion-worker/src/parts/ExtensionHostEditor/ExtensionHostEditor.ts
+++ packages/completion-worker/src/parts/ExtensionHostEditor/ExtensionHostEditor.ts
@@ -1,9 +1,8 @@
-import { ExtensionHost } from '@lvce-editor/rpc-registry'
-import * as ActivateByEvent from '../ActivateByEvent/ActivateByEvent.ts'
+import { EditorWorker, ExtensionHost } from '@lvce-editor/rpc-registry'
 
 export const execute = async ({ args, editorLanguageId, editorUid, event, method }: any) => {
   const fullEvent = `${event}:${editorLanguageId}`
-  await ActivateByEvent.activateByEvent(fullEvent)
+  await EditorWorker.activateByEvent(fullEvent)
   const result = await ExtensionHost.invoke(method, editorUid, ...args)
   return result
 }
