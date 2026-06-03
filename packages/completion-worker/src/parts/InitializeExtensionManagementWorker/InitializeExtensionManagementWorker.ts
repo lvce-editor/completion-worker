35b95138b555552c30fa16aa1625a132cfaba09e
Le Vivilet <levivilet@pm.me>
2026-06-03 21:49:43 +0200

    feature: update initializeExtensionManagementWorker to use LazyTransferMessagePortRpcParent and add send function

--- packages/completion-worker/src/parts/InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts
+++ packages/completion-worker/src/parts/InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts
@@ -1,10 +1,16 @@
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
-import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
+import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
+import { EditorWorker, ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 
+const send = async (port: MessagePort) => {
+  // TODO
+  await EditorWorker.sendMessagePortToExtensionHostWorker(port)
+}
+
 export const initializeExtensionManagementWorker = async (): Promise<void> => {
-  const rpc = await WebWorkerRpcClient.create({
+  const rpc = await LazyTransferMessagePortRpcParent.create({
     commandMap: CommandMap.commandMap,
+    send,
   })
   ExtensionManagementWorker.set(rpc)
 }
