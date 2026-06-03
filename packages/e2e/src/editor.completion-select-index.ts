9bd1d7750bd954937fe52f05641a8ddd59c34ee4
Le Vivilet <levivilet@pm.me>
2026-06-03 21:53:49 +0200

    e2e

--- packages/e2e/src/editor.completion-select-index.ts
+++ packages/e2e/src/editor.completion-select-index.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'editor.completion-select-index'
 
+export const skip = 1
+
 export const test: Test = async ({ Editor, EditorCompletion, expect, Extension, FileSystem, Locator, Main, Workspace }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/editor.completion-click')
