80e5a762c09e0ac3f795ebbec8a1afde53569e9e
Le Vivilet <levivilet@pm.me>
2026-06-03 19:09:55 +0200

    fix

--- packages/completion-worker/src/parts/Listen/Listen.ts
+++ packages/completion-worker/src/parts/Listen/Listen.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
-import { EditorWorker } from '@lvce-editor/rpc-registry'
-import * as CommandMap from '../CommandMap/CommandMap.ts'
-=======
 import { initializeEditorWorker } from '../InitializeEditorWorker/InitializeEditorWorker.ts'
->>>>>>> origin/main
 
 export const listen = async (): Promise<void> => {
   await initializeEditorWorker()
