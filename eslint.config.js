4d8cce777f8cbf6f09c7c99ad3f5d0e9b77c9081
Le Vivilet <levivilet@pm.me>
2026-06-03 19:41:23 +0200

    feature: enhance type safety and improve ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,6 @@
 import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
+import * as regex from '@lvce-editor/eslint-plugin-regex'
+import * as tsconfig from '@lvce-editor/eslint-plugin-tsconfig'
 
-export default [
-  ...config.default,
-  ...actions.default,
-  {
-    ignores: ['**/server/**', '**/e2e/**', '**/memory/**'],
-  },
-  {
-    rules: {
-      'no-constant-condition': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
-      'no-useless-escape': 'off',
-    },
-  },
-]
+export default [...config.default, ...actions.default, ...tsconfig.default, ...regex.default]
--- packages/completion-worker/src/parts/ExtensionHostEditor/ExtensionHostEditor.ts
+++ packages/completion-worker/src/parts/ExtensionHostEditor/ExtensionHostEditor.ts
@@ -1,6 +1,15 @@
 import { EditorWorker, ExtensionHost } from '@lvce-editor/rpc-registry'
 
-export const execute = async ({ args, editorLanguageId, editorUid, event, method }: any) => {
+interface ExecuteOptions {
+  readonly args: readonly any[]
+  readonly combineResults?: (result: any) => any
+  readonly editorLanguageId?: string
+  readonly editorUid: number
+  readonly event: string
+  readonly method: string
+}
+
+export const execute = async ({ args, editorLanguageId, editorUid, event, method }: ExecuteOptions): Promise<any> => {
   const fullEvent = `${event}:${editorLanguageId}`
   await EditorWorker.activateByEvent(fullEvent)
   const result = await ExtensionHost.invoke(method, editorUid, ...args)
