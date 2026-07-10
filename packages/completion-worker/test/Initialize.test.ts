a38322b890de4ef6a5e0d77daeeb2845e79023ea
Le Vivilet <levivilet@pm.me>
2026-07-10 11:53:19 +0200

    feature: update eslint config (#375)

--- packages/completion-worker/test/Initialize.test.ts
+++ packages/completion-worker/test/Initialize.test.ts
@@ -2,6 +2,5 @@ import { test, expect } from '@jest/globals'
 import { initialize } from '../src/parts/Initialize/Initialize.ts'
 
 test('initialize resolves', async () => {
-  await initialize()
-  expect(true).toBe(true)
+  await expect(initialize()).resolves.toBeUndefined()
 })
